2e0094c8fcdac1854ecfe20a748e14962e468c16
Sheik <sf354@njit.edu>
2018-10-22 15:48:11 -0400

    Created authentication in laravel

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -4,7 +4,7 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
-    <title>Laravel</title>
+    <title>Hello World</title>
 
     <!-- Fonts -->
     <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet" type="text/css">
@@ -81,7 +81,7 @@
 
     <div class="content">
         <div class="title m-b-md">
-            Welcome to the home page
+            About
         </div>
 
         <div class="links">
